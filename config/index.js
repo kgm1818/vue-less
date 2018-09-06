fd392c51b043ecc54c95d782aa0db9d82480551c
kangguimin@juooo.com <kangguimin@juooo.com>
2018-09-06 15:54:34 +0800

    添加配置

--- config/index.js
+++ config/index.js
@@ -1,1 +1,19 @@
-jk
+
+
+module.exports = {
+    entry: "../src/main.js",// 入口文件
+    output: {// 输出文件
+        publicPath: __dirname + '/build',// 添加静态资源，否则会出现路径错误
+        path: __dirname + '/build',// 输出文件路径
+        filename: '[name].js' // 输出文件名
+    },
+    module:{// 加载器
+        rules: [
+            {
+                test: '',
+                use: []
+            }
+        ]
+    }
+
+}

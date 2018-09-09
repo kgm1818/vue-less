0963398e47d438ceb0f9fa48fb48830b2a86e413
myphone <1634750252@example.com>
2018-09-09 20:21:41 +0800

    项目结构

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <router-view></router-view>
+    <nav>
+      <router-link>首页</router-link>
+      <router-link>工作</router-link>
+      <router-link>群聊</router-link>
+      <router-link>论坛</router-link>
+      <router-link>我的</router-link>
+    </nav>
+  </div>
 </template>
 <script>
 export default {
